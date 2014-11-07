b718870a683e0da8e461e8df8ffb443b2cc5c8ae
nanjekyejoannah <nanjekyejoannah@yahoo.com>
2014-11-07 14:25:08 +0300

    functions for the calculator added

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,8 @@ Shoes.app(title: "My calculator", width: 200, height: 240) do
 
 	number_field = nil
 	@number = 0
+	@op = nil
+  	@previous = 0
 	flow width: 200 , height: 240 do
 
 		flow width: 0.7, height: 0.2 do
@@ -26,11 +28,17 @@ Shoes.app(title: "My calculator", width: 200, height: 240) do
       				case btn
 			          when /[0-9]/ 
 			              @number = @number.to_i * 10 + btn.to_i
-			          end
-			          number_field.replace(@number)  
-      				end
+			          
+			          when '='
+              			@number = @previous.send(@op, @number)
+            		  else
+			              @previous, @number = @number, nil
+			              @op = btn
+			        end
+			        number_field.replace(@number)  
+      				
       			end
     		end
-
+    	end
 	end
 end
